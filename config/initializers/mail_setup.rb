2745e6d00078363098d2805e42359994a8df4f35
Hooopo <hoooopo@gmail.com>
2011-06-25 21:11:08 +0800

    config email

---
+++ config/initializers/mail_setup.rb
@@ -0,0 +1,17 @@
+if Rails.env != 'test'
+
+  c = YAML::load(File.open("#{Rails.root}/config/email.yml"))
+
+  ActionMailer::Base.smtp_settings = {
+    :address => c[Rails.env]['address'],
+    :port => c[Rails.env]['port'],
+    :domain => c[Rails.env]['domain'],
+    :authentication => c[Rails.env]['authentication'],
+    :user_name => c[Rails.env]['user_name'],
+    :password => c[Rails.env]['password'],
+    :enable_starttls_auto => c[Rails.env]["enable_starttls_auto"]    
+  }
+
+end
+
+ActionMailer::Base.default_url_options[:host] = "localhost:3000" 
