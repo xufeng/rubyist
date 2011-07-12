9ee6bd6ef657657ba3d72f165f1805d48cd005ce
Hooopo <hoooopo@gmail.com>
2011-07-13 02:42:43 +0800

    add pagination for user/articles

--- app/controllers/user/articles_controller.rb
+++ app/controllers/user/articles_controller.rb
@@ -2,12 +2,13 @@ class User::ArticlesController < User::BaseController
   before_filter :find_user
   
   def index
-    @articles = @user.articles
+    @articles = @user.articles.page(params[:page])
   end
 
   def show
     @article = @user.articles.find(params[:id])
     @comment = @article.comments.new
+    @comments = @article.comments.page(params[:page])
     render "articles/show"
   end
 
