1c9babaf85c45187d35be7aa72bdbc2d159fe2bd
Hooopo <hoooopo@gmail.com>
2011-07-13 02:39:22 +0800

    add pagination for articles in some tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,12 @@
 class TagsController < ApplicationController
   def index
+    #TODO ActsAsTaggableOn::Tag.page not work properly
     @tags = ActsAsTaggableOn::Tag.all
   end
 
   def show
     @tag = ActsAsTaggableOn::Tag.find_by_name(params[:id])
-    @articles = Article.tagged_with(@tag.name, :on => :tags)
+    @articles = Article.tagged_with(@tag.name, :on => :tags).page(params[:page])
   end
 
 end
