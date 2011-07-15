d201214f902142e8360ee4fe0e8fa324c55eb789
Hooopo <hoooopo@gmail.com>
2011-07-16 02:38:41 +0800

    add links for article tags

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,3 +1,7 @@
 module ArticlesHelper
   include ActsAsTaggableOn::TagsHelper
+  
+  def tag_with_link(article)
+    article.tags_on(:tags).map{|tag| link_to(tag.name, tag_path(tag))}.join(", ").html_safe
+  end
 end
