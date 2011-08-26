7643b12d216c19ed68f84efc4aac90d984eb3854
Hooopo <hoooopo@gmail.com>
2011-08-27 05:10:33 +0800

    add gist for code highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,15 @@ module ApplicationHelper
   end
 
   def format(text)
-    bbcode(coderay(text)).force_encoding("UTF-8").encode("UTF-8").html_safe
+    gist(bbcode(coderay(text)).force_encoding("UTF-8").encode("UTF-8")).html_safe
   end
 
   def avatar_url(token, size = 64)
     "http://gravatar.com/avatar/#{token}.png?s=#{size}&d=#{CGI.escape('http://assets.github.com/images/gravatars/gravatar-140.png')}"
+  end
+  
+  def gist(text)
+    text.gsub(/\[gist\]<a[^>]+>(https?:\/\/gist.github.com\/[\d]+)\.js<\/a>\[\/gist\]/, '<script src="\1.js"></script>')
   end
 
 
