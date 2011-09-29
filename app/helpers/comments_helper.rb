92817200489b876402eeb76adb403583e9bb3135
Hooopo <hoooopo@gmail.com>
2011-09-30 03:22:24 +0800

    fix render without template bug

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,7 @@
 module CommentsHelper
   def nested_comments(comments)
     comments.map do |comment, sub_comments|
-      render(comment, :article => @article, :comment => comment) + content_tag(:div, nested_comments(sub_comments), :class => "nested_comments")
+      render("comments/comment", :article => @article, :comment => comment) + content_tag(:div, nested_comments(sub_comments), :class => "nested_comments")
     end.join.html_safe
   end
 end
