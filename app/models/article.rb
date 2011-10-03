5637e9186f07fe51898a72b3a891eac629717a54
Hooopo <hoooopo@gmail.com>
2011-10-04 01:34:56 +0800

    enlarge article title length

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,7 +8,7 @@ class Article < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   belongs_to :user, :counter_cache => true
 
-  validates :title, :presence => true, :length => 1..25 
+  validates :title, :presence => true, :length => 1..100 
   validates :body,  :presence => true, :length => {:minimum => 10}
 
   delegate :nick_name,    :to => :user, :prefix => true, :allow_nil => true 
