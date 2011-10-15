25b936da4d44414856dd0ec585cb28b88b3dfddd
Hooopo <hoooopo@gmail.com>
2011-10-16 04:16:59 +0800

    list 20 articles in page index

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,5 @@
 class SiteController < ApplicationController
   def index
-    @articles = Article.includes(:user).hottest.visible_by_all.limit(15)
+    @articles = Article.includes(:user).hottest.visible_by_all.limit(20)
   end
 end
